8f394f3b55e988bec10dd9a64bd68cba977b24c7
Jason Mitchell <jasonmitchell@live.co.uk>
2013-11-03 10:41:41 +0000

    Setting up ninject controller factory

---
+++ src/Sample.Web/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace Sample.Web.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private readonly IKernel kernel;
+
+        public NinjectControllerFactory(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType != null ? kernel.Get(controllerType) as IController : null;
+        }
+    }
+}
